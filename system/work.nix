eedae7e5c5042371f216028f7c30720ac16c45be
Jan Malte Töpperwien <m.toepperwien@ai.uni-hannover.de>
2026-05-21 10:17:32 +0200

    feat(workpc): do not manually override nvidia driver anymore

--- system/work.nix
+++ system/work.nix
@@ -61,14 +61,6 @@
   };
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "580.126.18";
-      sha256_64bit = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
-      sha256_aarch64 = "sha256-pruxWQlLurymRL7PbR24NA6dNowwwX35p6j9mBIDcNs=";
-      openSha256 = "sha256-1Q2wuDdZ6KiA/2L3IDN4WXF8t63V/4+JfrFeADI1Cjg=";
-      settingsSha256 = "sha256-QMx4rUPEGp/8Mc+Bd8UmIet/Qr0GY8bnT/oDN8GAoEI=";
-      persistencedSha256 = lib.fakeSha256;
-    };
     modesetting.enable = true;
     open = true;
   };
