5f5dd4e54d7e633b25810bed4516352aaa550d5c
Jan Malte Töpperwien <m.toepperwien@ai.uni-hannover.de>
2026-07-03 10:19:44 +0200

    fix(boot): retry on wrong password for encryption

--- system/hardware/workpc_disko.nix
+++ system/hardware/workpc_disko.nix
@@ -33,7 +33,7 @@
                   "--perf-no_write_workqueue"
                 ];
                 # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
-                settings = {crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10"];};
+                settings = {crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10" "tries=0"];};
                 content = {
                   type = "btrfs";
                   extraArgs = ["-L" "nixos" "-f"];
